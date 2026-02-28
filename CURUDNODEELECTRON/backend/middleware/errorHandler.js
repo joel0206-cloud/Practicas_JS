dcb25dfac9ff4f1d087a4f2fc2fbc43312be581c
Joel Francisco Hernandez <joel.0206@itsjc.edu.mx>
2026-02-28 10:50:35 -0600

    Delete CURUDNODEELECTRON directory

--- CURUDNODEELECTRON/backend/middleware/errorHandler.js
+++
@@ -1,9 +0,0 @@
-const errorHandler = (err, req, res, next) => {
-  console.error(err);
-  const status = err.status || 500;
-  res.status(status).json({
-    message: err.message || 'Error interno del servidor'
-  });
-};
-
-module.exports = errorHandler;
